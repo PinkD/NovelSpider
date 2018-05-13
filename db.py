cda933c2ac0e2c67c8b2b51f0de43ad6a9cee026
PinkD <443657547@qq.com>
2018-05-14 00:03:04 +0800

    add retry on network fail & add existence check

--- db.py
+++ db.py
@@ -5,17 +5,17 @@ from novel import Novel
 
 class DatabaseHelper:
     def __init__(self, name='test'):
-        self.conn = sqlite3.connect(name + '.db', check_same_thread=False)
+        self._conn = sqlite3.connect(name + '.db', check_same_thread=False)
         self._init_db()
 
     def _init_db(self):
-        cursor = self.conn.cursor()
+        cursor = self._conn.cursor()
         # cursor.execute('DROP TABLE IF EXISTS `novel`')
         cursor.execute(
             '''
             CREATE TABLE IF NOT EXISTS `novel`(
               `id`          INTEGER PRIMARY KEY,
-              `title`       TEXT  NOT NULL  UNIQUE,
+              `title`       TEXT  NOT NULL,
               `author`      TEXT,
               `count`       INTEGER UNSIGNED,
               `description` TEXT,
@@ -24,16 +24,27 @@ class DatabaseHelper:
             '''
         )
         cursor.close()
-        self.conn.commit()
+        self._conn.commit()
 
     def insert_novel(self, novel: Novel):
-        cursor = self.conn.cursor()
+        cursor = self._conn.cursor()
         cursor.execute(
             '''
             INSERT INTO `novel` (id, title, author, count, description, type) VALUES (?, ?, ?, ?, ?, ?)
-
             '''
             , (novel.id, novel.title, novel.author, novel.count, novel.description, novel.type)
         )
         cursor.close()
-        self.conn.commit()
+        self._conn.commit()
+
+    def check_novel_exists(self, id):
+        cursor = self._conn.cursor()
+        cursor.execute(
+            '''
+            SELECT id FROM `novel` WHERE `id` = ?
+            '''
+            , (id,)
+        )
+        result = cursor.fetchall()
+        cursor.close()
+        return result
--- spider.py
+++ spider.py
@@ -22,6 +22,8 @@ class NovelSpider:
         self._opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor())
         self._opener.addheaders.clear()
         self._opener.addheaders.append(('User-Agent', USER_AGENT))
+        self.retry_times = 5
+        self.log_file = open('status.log', 'w')
         socket.setdefaulttimeout(10)
         self._pool = ThreadPoolExecutor()
         if not os.path.exists('novels'):
@@ -29,33 +31,45 @@ class NovelSpider:
 
     @staticmethod
     def process_novel(self, novel: Novel, url: str):
-        result = self._opener.open(url).read().decode()
-        print(novel)
-        novel.count = int(re.findall('<div>&nbsp;总字数：([0-9]*?)</div>', result).pop())
-        novel.type = re.findall('<div>类别：<a href="/wapsort/[0-9]*?_[0-9]*?\.html" title=".*?">(.*?)</a></div>', result).pop()
-        page = BASE_URL + re.findall('<a href="(/novel/[0-9]*?/[0-9]*?\.html)" title="' + PAGE + '">1</a>', result).pop()
-        max_page = int(re.findall('<span style="color:#666;font-size:11px;line-height: 22px;">共([0-9]*?)章节</span>', result).pop())
-        # print(page)
-        result = self._opener.open(page).read().decode()
-        with open('novels/%d_%s.txt' % (novel.id, novel.title), 'w') as f:
+        retry_time = 0
+        while retry_time < self.retry_times:
             try:
-                for i in range(1, max_page - 1):
-                    content = re.findall('<div id="nr1" style="font-size:18px;">([\s\S]*?)</div>', result).pop().replace('</p>\r\n<p></p>', '').replace('<p>', '').replace('</p>', '').replace('&nbsp;', ' ')
-                    # print(content)
-                    print('%s->%d/%d' % (novel.title, i, max_page))
-                    f.write(content)
-                    page = re.findall('<td class="next"><a id="pt_next" href="(' + BASE_URL + '/novel/[0-9]*?/[0-9]*?\.html)">下一章</a></td>', result).pop()
-                    # print(page)
-                    result = self._opener.open(page).read().decode()
-                    time.sleep(1)
-                self._db.insert_novel(novel)
-                print(novel.title + ' Done')
+                result = self._opener.open(url).read().decode()
             except Exception as e:
                 print(e)
+                continue
+            # print(novel)
+            novel.count = int(re.findall('<div>&nbsp;总字数：([0-9]*?)</div>', result).pop())
+            novel.type = re.findall('<div>类别：<a href="/wapsort/[0-9]*?_[0-9]*?\.html" title=".*?">(.*?)</a></div>', result).pop()
+            page = BASE_URL + re.findall('<a href="(/novel/[0-9]*?/[0-9]*?\.html)" title="' + PAGE + '">1</a>', result).pop()
+            max_page = int(re.findall('<span style="color:#666;font-size:11px;line-height: 22px;">共([0-9]*?)章节</span>', result).pop())
+            # print(page)
+            result = self._opener.open(page).read().decode()
+            with open('novels/%d_%s.txt' % (novel.id, novel.title), 'w') as f:
+                try:
+                    for i in range(1, max_page - 1):
+                        content = re.findall('<div id="nr1" style="font-size:18px;">([\s\S]*?)</div>', result).pop().replace('</p>\r\n<p></p>', '').replace('<p>', '').replace('</p>', '').replace('&nbsp;', ' ')
+                        # print(content)
+                        print('%s->%d/%d' % (novel.title, i, max_page))
+                        f.write(content)
+                        page = re.findall('<td class="next"><a id="pt_next" href="(' + BASE_URL + '/novel/[0-9]*?/[0-9]*?\.html)">下一章</a></td>', result).pop()
+                        # print(page)
+                        result = self._opener.open(page).read().decode()
+                        time.sleep(1)
+                    self._db.insert_novel(novel)
+                    print(novel.title + ' Done')
+                    self.log_file.write(novel.title + ' Done')
+                    return
+                except Exception as e:
+                    print(e)
+            retry_time += 1
+        print("!!!Fail to save %s" % novel.title)
+        self.log_file.write("!!!Fail to save %s" % novel.title)
 
     def start(self):
         result = self._opener.open(BASE_URL + '/wapsort/11_1.html').read().decode()
         max_page = int(re.findall('<input style="width: 50%;" type="number" name="page" value="" id="go_page" min="1" max="([0-9]*?)" />', result).pop())
+        # max_page = 5
         for i in range(1, max_page):
             result = self._opener.open(BASE_URL + '/wapsort/11_%d.html' % i).read().decode()
             ids = re.findall('<h3><a href="' + BASE_URL + '/novel/([0-9]*)\.html">.*?</a></h3>', result)
@@ -65,4 +79,7 @@ class NovelSpider:
             descriptions = re.findall('<span class="abstract"><a href="' + BASE_URL + '/novel/[0-9]*\.html">([\s\S]*?)</a></span>', result)
             for j in range(0, titles.__len__()):
                 novel = Novel(titles[j], authors[j], descriptions[j], id=int(ids[j]))
+                if self._db.check_novel_exists(novel.id):
+                    print("Skip existing: %s" % novel.title)
+                    continue
                 self._pool.submit(self.process_novel, self, novel, urls[j])
